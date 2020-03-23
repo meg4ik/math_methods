66f80f16bb034dcc30827b36534f926f006d967f
meg4ik <meg4mag@gmail.com>
2020-03-23 18:16:54 +0200

    🔗

---
+++ Gauss method.cpp
@@ -0,0 +1,85 @@
+﻿#include <math.h>
+#include <iostream>
+using namespace std;
+
+void glavelem(int k, double mas[][5 + 1], int n, int otv[])
+{
+	int i, j, i_max = k, j_max = k;
+	double temp;
+
+	for (i = k; i < n; i++)
+		for (j = k; j < n; j++)
+			if (fabs(mas[i_max][j_max]) < fabs(mas[i][j]))
+			{
+				i_max = i;
+				j_max = j;
+			}
+
+	for (j = k; j < n + 1; j++)
+	{
+		temp = mas[k][j];
+		mas[k][j] = mas[i_max][j];
+		mas[i_max][j] = temp;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		temp = mas[i][k];
+		mas[i][k] = mas[i][j_max];
+		mas[i][j_max] = temp;
+	}
+
+	i = otv[k];
+	otv[k] = otv[j_max];
+	otv[j_max] = i;
+}
+
+int main()
+{
+	double mas[5][5 + 1];
+	double x[5];
+	int otv[5];
+	int i, j, k, n;
+
+	do
+	{
+		cout << "Enter the number of system equations(2-5) : ";
+		cin >> n;
+	} while (5 < n);
+	cout << "Enter numbers:\n";
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n + 1; j++)
+			cin >> mas[i][j];
+
+	for (i = 0; i < n + 1; i++)
+		otv[i] = i;
+
+
+	for (k = 0; k < n; k++)
+	{
+		glavelem(k, mas, n, otv);
+
+		for (j = n; j >= k; j--)
+			mas[k][j] /= mas[k][k];
+		for (i = k + 1; i < n; i++)
+			for (j = n; j >= k; j--)
+				mas[i][j] -= mas[k][j] * mas[i][k];
+	}
+
+	for (i = 0; i < n; i++)
+		x[i] = mas[i][n];
+	for (i = n - 2; i >= 0; i--)
+		for (j = i + 1; j < n; j++)
+			x[i] -= x[j] * mas[i][j];
+
+
+	cout << endl;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			if (i == otv[j])
+			{
+				cout << x[j] << endl;
+				break;
+			}
+	return 0;
+}
---
+++ Kramer method.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double det(int n, double A[4][4])
+{
+	if (n == 1) {
+		return A[0][0];
+	}
+	else {
+		double M1[4][4];
+		double r = 0;
+		int i, iterator, j, k;
+		for (i = 0; i < n; i++) {
+			for (k = 1; k < n; k++) {
+				iterator = 0;
+				for (j = 0; j < n; j++) {
+					if (j != i)
+						M1[k - 1][iterator++] = A[k][j];
+				}
+			}
+			if (i % 2 == 0) {
+				r += A[0][i] * det(n - 1, M1);
+			}
+			else {
+				r -= A[0][i] * det(n - 1, M1);
+			}
+		}
+		return r;
+	}
+}
+
+int main() {
+	double A[4][4];
+	double mod[4];
+	double buf[4];
+	double B[4];
+
+	cout << "Enter A-matrix: " << endl;
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 4; j++)
+		{
+			cout << "A [" << i + 1 << " , " << j + 1 << "] =  ";
+			cin >> A[i][j];
+		}
+	cout << endl;
+	cout << "Enter B-matrix: " << endl;
+	for (int i = 0; i < 4; i++)
+	{
+		cout << "B [" << i + 1 << "] = ";
+		cin >> B[i];
+	}
+	double del = det(4, A);
+
+	for (int i = 0; i < 4; i++)
+	{
+		buf[i] = A[i][0];
+		A[i][0] = B[i];
+	}
+	mod[0] = det(4, A);
+
+	for (int k = 0; k < 3; k++)
+	{
+		for (int j = 0; j < 4; j++)
+			A[j][k] = buf[j];
+		for (int i = 0; i < 4; i++)
+		{
+			buf[i] = A[i][k + 1];
+			A[i][k + 1] = B[i];
+		}
+		mod[k + 1] = det(4, A);
+	}
+
+	cout << endl;
+	for (int k = 0; k < 4; k++)
+	{
+		cout << "x" << (k + 1) << " = " << round(mod[k] / del * 1000) / 1000 << endl;
+	}
+	return 0;
+}
