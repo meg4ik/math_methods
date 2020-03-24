8b1baa7bb8e5963a2fe3822d97420fcaba916742
meg4ik <meg4mag@gmail.com>
2020-03-24 13:15:49 +0200

    Create method_of_golden_section.cpp
    ONE-DIMENSIONAL SEARCH OF OPTIMUM. METHODS OF OPTIMIZATION WITH EXCEPTION OF INTERVALS

---
+++ method_of_golden_section.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+const double goldenRatio = (1 + sqrt(5)) / 2;
+
+
+double foo(double x)
+{
+	return (2 * (x * x)) + (3 * exp(-x));
+}
+
+int main() {
+	double a, b;
+	double accuracy = 0.001;
+	double x1, x2;
+	cin >> a >> b;
+	while (fabs(b - a) > accuracy) {
+		x1 = b - (b - a) / goldenRatio;
+		x2 = a + (b - a) / goldenRatio;
+		if (foo(x1) <= foo(x2))
+			a = x1;
+		else
+			b = x2;
+	}
+	cout << "(" << (a + b) / 2 << ", " << foo((a + b) / 2) << ")";
+	return 0;
+}
