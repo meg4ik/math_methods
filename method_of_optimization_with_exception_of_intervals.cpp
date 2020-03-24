c25b8ee93f54a7d098fd3ef3e116c3ec5df0365a
meg4ik <meg4mag@gmail.com>
2020-03-24 13:06:37 +0200

    Create method_of_optimization_with_exception_of_intervals.cpp
    ONE-DIMENSIONAL SEARCH OF OPTIMUM. METHODS OF OPTIMIZATION WITH EXCEPTION OF INTERVALS

---
+++ method_of_optimization_with_exception_of_intervals.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double foo(double x)
+{
+    return (2 * (x * x)) + (3 * exp(-x));
+}
+
+int main()
+{
+    double a, b, L, xm;
+    cin >> a >> b;
+    L = b - a;
+    xm = (b + a) / 2.;
+    double x1, x2;
+    while (L > 0.001) {
+        x1 = a + L / 4.;
+        x2 = b - a / 4.;
+        if (foo(x1) < foo(xm)) {
+            b = xm;
+            xm = x1;
+        }
+        else if(foo(xm)<foo(x2)) {
+            a = x1;
+            b = x2;
+        }
+        else {
+            a = xm;
+            xm = x2;
+        }
+            L = b - a;  
+    }
+    cout << L << "\n" << xm << "\n" << foo(xm);
+}
